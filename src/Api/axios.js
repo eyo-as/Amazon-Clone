1c5b13f7154ac3ead7b1f8805355352b2840aa2c
eyo-as <eyosiyastumiso7@gmail.com>
2024-07-25 02:31:05 +0300

    api change

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-8b029/us-central1/api",
+  // localhost
+  // baseURL: "http://127.0.0.1:5001/clone-8b029/us-central1/api",
+
+  // deployed version
+  baseURL: "https://amazon-api-deploy-1270.onrender.com",
 });
 
 export { AxiosInstance };
